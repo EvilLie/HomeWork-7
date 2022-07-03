0140bb141511aed0447a0d62b872fa642c7ff31e
Artemiy Sadriev <mosin1898@mail.ru>
2022-07-03 13:24:31 +0300

    Добавьте файлы проекта.

---
+++ HomeWork№7/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_7
+{
+    public struct Employee
+    {
+        /// <summary>
+        /// Employees number in catalog
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// Date when record was created
+        /// </summary>
+        public DateTime DateOfCreating{ get; set; }
+        /// <summary>
+        /// Full name of employee
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Age of employee
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// Height of employee
+        /// </summary>
+        public int Height { get; set; }
+        /// <summary>
+        /// Employees date of birth
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+        /// <summary>
+        /// Place of dirth of employee
+        /// </summary>
+        public string PlaceOfBirth { get; set; }
+    }
+}
---
+++ HomeWork№7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_7
+{
+    internal class Program
+    {
+        public static void Text(string s)
+        {
+            Console.WriteLine(s);
+        }
+        public static Employee InitEmployee(int ID)
+        {
+            Employee employee = new Employee();
+            employee.ID = ID;
+            employee.DateOfCreating = DateTime.Now;
+            Text("Please enter full name of emloyee: ");
+            employee.FullName = $"{Console.ReadLine()}#";
+            Text("Please enter age of new employee: ");
+            employee.Age = Convert.ToInt32($"{Console.ReadLine()}#");
+            Text("Please enter height of new employee: ");
+            employee.Height = Convert.ToInt32($"{Console.ReadLine()}#");
+            Text("Enter date of birth: DD.MM.YYYY");
+            employee.DateOfBirth = Convert.ToDateTime("{Console.ReadLine()}#");
+            Text("Enter place of birth");
+            employee.PlaceOfBirth = $"{Console.ReadLine()}#";
+            return employee;
+        }
+        public static string Employee(Employee employee)
+        {
+            string s = $"{employee.ID}#" + $"{employee.DateOfCreating}#" + employee.FullName + employee.Age + employee.Height + employee.DateOfBirth + employee.PlaceOfBirth; 
+            return s;
+        }
+        public static void WriteToFile(string fileName, string s)
+        {
+            File.AppendAllText(fileName, s);
+        }     
+        static void Main(string[] args)
+        {
+            int ID = 1;
+        }
+    }
+}
